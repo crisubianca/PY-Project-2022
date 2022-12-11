f5800d1a4e1445b6d425fcfb81b849310efa2ea3
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-12-11 19:55:24 +0200

    scraped links, names & ingredients

--- main.py
+++ main.py
@@ -1,48 +1,82 @@
 import requests
+from bs4 import BeautifulSoup
+import re
 
 directory = "recipes/"
 # f = open(directory + "test.txt", 'w')
 #
 # f.write("test")
 # f.close()
+recipesLinksFile = "links.txt"
+categories = ['aperitive', 'ciorbe-si-supe', 'conserve-muraturi', 'dulciuri', 'mancaruri', 'paine', 'salate', 'torturi',
+              'diverse']
 
-categories = ['aperitive', 'ciorbe-si-supe', 'conserve-muraturi', 'dulciuri', 'mancaruri', 'paine', 'salate', 'torturi', 'diverse']
 
 def getRecipesLinksByCategory(category):
     link = "https://jamilacuisine.ro/retete-video/" + category
+    recipesLinks = []
     page = requests.get(link)
-    pageSource = page.text
-    pageSource = pageSource.split('\n')
+    # pageSource = page.text
+    # pageSource = pageSource.split('\n')
     print("URL:", link)
     print("******************\n")
 
-    for line in pageSource:
-        if line.find("<div class=\"tdb_module_loop td_module_wrap td-animation-stack td-cpt-post\"") != -1:
-            if line.find("<div class=\"td-module-container td-category-pos-\"") != -1:
-                if line.find("<div class=\"td-image-container\"") != -1:
-                    if line.find("<div class=\"td-module-thumb\"") != -1:
-                        if line.find("<a href=\"https://jamilacuisine.ro/") != -1:
-                            # line = line[26:]
-                            # line = line[:len(line) - 274]
-                            print(line)
-
-def getRecipesLinks():
-    recipesLinks= []
-
-    for c in categories:
-        recipesLinks.extend(getRecipesLinks(c))
+    soup = BeautifulSoup(page.text, 'html.parser')
+    for line in soup.find_all("div", {"class": "tdb_module_loop td_module_wrap td-animation-stack td-cpt-post"}):
+        # for line in soup.find_all("a"):
+        #     data = line.get('href')
+        #     print(str(data))
+        result = re.search('(?<=href=").*?(?=" rel=)', str(line))
+        recipesLinks.append(result.group())
+        # print(result.group())
+    # print(recipesLinks)
 
     return recipesLinks
 
-def writeLinks():
-    recipesLinks = getRecipesLinks()
-    f = open(directory + "links.txt", 'a')
+
+def getRecipesLinks():
+    recipesLinks = []
 
     for c in categories:
+        recipesLinks.extend(getRecipesLinksByCategory(c))
+
+    return recipesLinks
+
+
+def writeLinks():
+    recipesLinks = getRecipesLinks()
+    f = open(directory + recipesLinksFile, 'w')
+
+    for c in recipesLinks:
         f.write(c + '\n')
 
     f.close()
 
 
-getRecipesLinksByCategory('aperitive')
+recipeUsed = "https://jamilacuisine.ro/ciorba-de-fasole-verde-de-post-reteta-video/"
 
+
+def scrapeRecipe(recipeUsed):
+    page = requests.get(recipeUsed)
+    print("URL:", recipeUsed)
+    print("******************\n")
+    ingredients = []
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+    title = soup.find_all("h1", {"class": "tdb-title-text"})
+    for ti in title:
+        print(ti.text)
+
+    ingred = soup.find_all("div", {"class": "wprm-recipe-ingredient-group"})
+    for ing in ingred:
+        ingredients.append(ing.text)
+
+    print(ingredients)
+
+    # fileName = res.text + ".txt"
+
+
+scrapeRecipe(recipeUsed)
+
+# getRecipesLinksByCategory('aperitive')
+# writeLinks()
