3d9b04c5b6879c5cc8d79c80d9a2d0b16112ca11
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-12-15 04:53:09 +0200

    updates......

--- main.py
+++ main.py
@@ -3,24 +3,24 @@ import requests
 from re import sub
 import re
 import json
+import pdb
 
 directory = "recipes/"
-# https://jamilacuisine.ro/dovlecei-pane-in-crusta-de-pesmet-si-in-aluat/
 
-def snake_case(s):
-    return '-'.join(
-        sub('([A-Z][a-z]+)', r' \1',
-            sub('([A-Z]+)', r' \1',
-                s.replace('-', ' '))).split()).lower()
+recipe = {} # it's not an actual recipe, more like a list of shopping cart
 
-def scrapeRecipe():
-    link = input("Insert you recipe here -> ")
+def custom_merge(dictionary, subdictionary, ingredient_name):
+    if ingredient_name in dictionary and dictionary[ingredient_name]['unit'] == subdictionary['unit']:
+        try:
+            dictionary[ingredient_name]['amount'] = float(dictionary[ingredient_name]['amount']) + float(subdictionary['amount'])
+        except ValueError:
+            pass
+    else:
+        dictionary[ingredient_name] = subdictionary
+
+def scrapeRecipe(link):
     page = requests.get(link)
-    # print("\nURL:", link)
-    # print("******************")
     soup = BeautifulSoup(page.text, 'html.parser')
-
-    recipe = {}
 
     # titlul retetei
     title = soup.select('.tdb-title-text')[0].text.strip().replace("/", "").split("–")[0]
@@ -29,80 +29,46 @@ def scrapeRecipe():
     # grupele de ingrediente -> ex. vezi reteta cu dovlecei, are 2 : pt dovlecei in aluat/crusta de pesmet
     ingredient_groups = soup.select('.wprm-recipe-ingredient-group')
 
-    if len(ingredient_groups) == 1:
-        try:
-            # linia pe care se afla un ingredient dintr un grup de ingrediente
-            ingredients_unparsed = ingredient_groups[0].select('.wprm-recipe-ingredients > .wprm-recipe-ingredient')
-        except:
-            return
-
-        all_ingredients = []
-        
-    #      extragem informatiile despre fiecare ingredient: amount, unit, name
-        for i in ingredients_unparsed:
-            # ingredient amount
+    for j in ingredient_groups:
+        group_ingredients_unparsed = j.select('.wprm-recipe-ingredients > .wprm-recipe-ingredient')
+        for i in group_ingredients_unparsed:
             ingredient_amount = i.select('.wprm-recipe-ingredient-amount')
             ingredient_amount_parsed = ''
             if ingredient_amount:
                 ingredient_amount_parsed = i.select('.wprm-recipe-ingredient-amount')[0].text.strip()
 
-            # ingredient unit
             ingredient_unit = i.select('.wprm-recipe-ingredient-unit')
             ingredient_unit_parsed = ''
             if ingredient_unit:
                 ingredient_unit_parsed = i.select('.wprm-recipe-ingredient-unit')[0].text.strip()
 
-            # ingredient name
             ingredient_name = i.select('.wprm-recipe-ingredient-name')
             ingredient_name_parsed = ''
             if ingredient_name:
                 ingredient_name_parsed = i.select('.wprm-recipe-ingredient-name')[0].text.strip()
 
             # construim ingredientul
-            ingredient = ingredient_amount_parsed + " " + ingredient_unit_parsed + " " + ingredient_name_parsed
-            # adaugam ingredientul construit in lista de ingrediente
-            all_ingredients.append(ingredient)
+            ingredient = {'amount': ingredient_amount_parsed, 'unit': ingredient_unit_parsed}
+            # i = ingredient_amount_parsed + " " + ingredient_unit_parsed + " " + ingredient_name_parsed
 
-        #   reteta cu toate ingredientele
-        recipe['recipeName'] = title
-        recipe['ingredients'] = all_ingredients
+            custom_merge(recipe, ingredient, ingredient_name_parsed)
 
-    # similar pt retetele cu mai multe grupuri -> ex. vezi reteta cu dovlecei, are 2 : pt dovlecei in aluat/crusta de pesmet
-    if len(ingredient_groups) > 1:
 
-        for j in ingredient_groups:
-            try:
-                group_name = j.select('.wprm-recipe-group-name')[0].text.strip()
-            except:
-                return
-            group_ingredients_unparsed = j.select('.wprm-recipe-ingredients > .wprm-recipe-ingredient')
-            group_ingredients = []
-            for i in group_ingredients_unparsed:
-                ingredient_amount = i.select('.wprm-recipe-ingredient-amount')
-                ingredient_amount_parsed = ''
-                if ingredient_amount:
-                    ingredient_amount_parsed = i.select('.wprm-recipe-ingredient-amount')[0].text.strip()
-
-                ingredient_unit = i.select('.wprm-recipe-ingredient-unit')
-                ingredient_unit_parsed = ''
-                if ingredient_unit:
-                    ingredient_unit_parsed = i.select('.wprm-recipe-ingredient-unit')[0].text.strip()
-
-                ingredient_name = i.select('.wprm-recipe-ingredient-name')
-                ingredient_name_parsed = ''
-                if ingredient_name:
-                    ingredient_name_parsed = i.select('.wprm-recipe-ingredient-name')[0].text.strip()
-
-                i = ingredient_amount_parsed + " " + ingredient_unit_parsed + " " + ingredient_name_parsed
-                group_ingredients.append(i)
-
-            recipe['recipeName'] = title.split("–")[0]
-            recipe[group_name] = group_ingredients
-
-    json_string = json.dumps(recipe, indent=4)
-    with open(f"recipes/{snake_case(title)}", "w") as out_file:
+def writeInJson(shopping_cart):
+    json_string = json.dumps(shopping_cart, indent=4)
+    with open(f"recipes/ShoppingCart", "w") as out_file:
         out_file.write(json_string)
 
 
-scrapeRecipe()
+def main():
+    links = ['https://jamilacuisine.ro/dovlecei-pane-in-crusta-de-pesmet-si-in-aluat/',
+             'https://jamilacuisine.ro/chec-pufos-cu-cacao-reteta-video/',
+             'https://jamilacuisine.ro/budinca-de-dovlecei-cu-bacon-reteta-video/']
+    for link in links:
+        scrapeRecipe(link)
+
+    writeInJson(recipe)
+
+
+main()
 
