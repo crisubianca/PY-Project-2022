328bb38d64a0ddf2ef97d764bb81ab8a1773a966
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-12-17 18:03:07 +0200

    final

--- main.py
+++ main.py
@@ -12,29 +12,27 @@ directory = "recipes/"
 
 recipe = {} # it's not an actual recipe, more like a list of shopping cart
 
+
 def custom_merge(dictionary, subdictionary, ingredient_name):
-    if ingredient_name in dictionary :
+    if ingredient_name in dictionary:
         for i in range(len(dictionary[ingredient_name])):
             if dictionary[ingredient_name][i]['unit'] == subdictionary['unit']:
                 try:
-                    try:
-                        # subdictionary_clean = subdictionary['amount'].strip().replace("-", ".").split(".")[0]
-                        dictionary[ingredient_name][i]['amount'] = float(
-                            dictionary[ingredient_name][i]['amount']) + float(subdictionary['amount'])
-                    except ValueError as error:
-                        for key, value in subdictionary.items():
-                            # print(f'{key}: {type(value)}')
-                            if type(value) == str:
-                                dict = {**dictionary[ingredient_name][i], **subdictionary}
-                                dictionary[ingredient_name][i]['amount'] = dict['amount']
+                    dictionary[ingredient_name][i]['amount'] = float(dictionary[ingredient_name][i]['amount']) + float(
+                        subdictionary['amount'])
                 except ValueError as error:
                     print(error)
-                    dictionary[ingredient_name] = [subdictionary]
-            else:
+                break
+            if i == len(dictionary[ingredient_name]) - 1 and dictionary[ingredient_name][i]['unit'] != subdictionary[
+                'unit']:
                 dictionary[ingredient_name].append(subdictionary)
+                dictionary[ingredient_name][-1]['amount'] = float(dictionary[ingredient_name][-1]['amount'])
+
 
     else:
-        dictionary[ingredient_name] =[subdictionary]
+
+        dictionary[ingredient_name] = [subdictionary]
+        dictionary[ingredient_name][-1]['amount'] = float(dictionary[ingredient_name][-1]['amount'])
 
 
 def scrapeRecipe(link):
@@ -67,12 +65,18 @@ def scrapeRecipe(link):
                 ingredient_name_parsed = i.select('.wprm-recipe-ingredient-name')[0].text.strip()
 
             # construim ingredientul
-            ingredient = {'amount': ingredient_amount_parsed, 'unit': ingredient_unit_parsed}
-            # i = ingredient_amount_parsed + " " + ingredient_unit_parsed + " " + ingredient_name_parsed
-            # ingredient_informations = []
-            # ingredient_informations.append(ingredient)
-            # print(recipe)
-            # custom_merge(recipe, ingredient_informations, ingredient_name_parsed)
+
+            # verificam daca avem 3-4 in amount
+            try:
+                ingredient_amount_parsed = ingredient_amount_parsed.split('-')[-1]
+            except ValueError:
+                print(ValueError)
+
+            if ingredient_amount_parsed == 'putina' or ingredient_amount_parsed == 'putin':
+                ingredient_amount_parsed = 1
+            ingredient = {'amount': ingredient_amount_parsed if ingredient_amount_parsed else 1,
+                          'unit': ingredient_unit_parsed if ingredient_unit_parsed else 'bucata'}
+
             custom_merge(recipe, ingredient, ingredient_name_parsed)
 
 
